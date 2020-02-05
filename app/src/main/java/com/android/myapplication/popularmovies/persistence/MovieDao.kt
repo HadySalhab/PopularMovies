0befbfbbc1f51fadfeba7c807922a5f798a66d3d
Hady Salhab <hadysalhab1@gmail.com>
2020-02-05 20:00:54 +1100

    fix: Fix MovieDao Entity spelling

--- app/src/main/java/com/android/myapplication/popularmovies/persistence/MovieDao.kt
+++ app/src/main/java/com/android/myapplication/popularmovies/persistence/MovieDao.kt
@@ -14,6 +14,6 @@ public interface MovieDao {
     @Query("SELECT * FROM MovieEntity WHERE id = :id")
     suspend fun getMovieById(id: Long): MovieEntity?
 
-    @Query("SELECT * FROM movieentity")
+    @Query("SELECT * FROM MovieEntity")
     fun getAllMovies(): LiveData<List<MovieEntity>>
 }
